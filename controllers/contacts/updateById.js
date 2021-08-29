d8a7b96aa230c46e8af7905e3b279d9605583b42
Yuliia Burko <yuliia.burko@gmail.com>
2021-08-29 23:46:20 +0200

    MongoDB

--- controllers/contacts/updateById.js
+++ controllers/contacts/updateById.js
@@ -1,19 +1,11 @@
-const contactsOperations = require('../../model/contacts')
-const { contactSchema } = require('../../validation')
+const { Contact } = require('../../models')
 
 const updateById = async (req, res, next) => {
   try {
-    const { error } = contactSchema.validate(req.body)
-    if (error) {
-      return res.status(400).json({
-        message: error.message,
-      })
-    }
     const { contactId } = req.params
-    const updateContact = await contactsOperations.updateContact(
-      contactId,
-      req.body,
-    )
+    const updateContact = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    })
     if (!updateContact) {
       return res.status(404).json({
         message: 'Not found',
