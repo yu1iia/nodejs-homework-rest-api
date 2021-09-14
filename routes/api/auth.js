a5574e9f61e401bf09fbe73f1924a736df31fabf
Yuliia Burko <yuliia.burko@gmail.com>
2021-09-15 01:09:58 +0200

    added avatar upload

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const { joiUserSchema } = require('../../models/user')
 const ctrl = require('../../controllers/auth')
-const { validation, authenticate } = require('../../middlewares')
+const { validation, authenticate, upload } = require('../../middlewares')
 
 const router = express.Router()
 
@@ -16,4 +16,10 @@ router.get('/logout', authenticate, ctrl.logout)
 
 router.get('/current', authenticate, ctrl.getCurrentUser)
 
+router.patch(
+  '/avatars',
+  authenticate,
+  upload.single('avatar'),
+  ctrl.updateAvatar,
+)
 module.exports = router
--- routes/api/current.js
+++
@@ -1,5 +0,0 @@
-// const express = require('express')
-// const { validation } = require('../../middlewares')
-// const ctrl = require('../../controllers/auth')
-
-// const { joiUserSchema } = require('../../models/current')
